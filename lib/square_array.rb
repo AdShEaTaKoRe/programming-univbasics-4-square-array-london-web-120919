04ea3915733aff374dbf0b5a357fb499af17f2db
Tommy Berman <tomy.berman1@gmail.com>
2019-11-26 14:06:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(numbers)
   counter = 0
-  while counter < numebers.length do
-    new_array = numbers**2
+  while counter < numbers.length do
+    #new_array = numbers**2
     #puts new_array
+    numbers**2
     counter += 1
 end
